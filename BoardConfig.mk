380e19caa4b2cd70962130cce0711e832cc6ee02
Bindu Gokul <bgokul@codeaurora.org>
2018-08-30 14:58:18 +0530

    msm8996: Set partitions for loop device.
    AdoptableHostTest in CTS uses virtual disk feature for testing.
    This change is to enable partitions for virtual disk.
    
    Change-Id: Ie03766738715ef54d4fa768033a96fdd76d3172b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,7 +113,8 @@ else
     BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0
 endif
 
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+
 endif
 
 BOARD_SECCOMP_POLICY := device/qcom/$(TARGET_BOARD_PLATFORM)/seccomp
