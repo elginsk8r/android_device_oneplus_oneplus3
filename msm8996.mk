988fc13d8755848058bd25e86a1976ecbcc2f779
padarshr <padarshr@codeaurora.org>
2018-10-17 16:59:15 +0530

    Clean-up the no longer supported logdumpd feature.
    Change-Id: I370fc49e7f37ac1ff81a42f3d41e8cd5cd29b9f5

--- msm8996.mk
+++ msm8996.mk
@@ -240,20 +240,6 @@ PRODUCT_PACKAGES += \
 #PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
         $(PRODUCT_PACKAGE_OVERLAYS)
 
-# Enable logdumpd service only for non-perf bootimage
-ifeq ($(findstring perf,$(KERNEL_DEFCONFIG)),)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
-        PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=0
-    else
-        #PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=1
-    endif
-else
-    PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-        ro.logdumpd.enabled=0
-endif
-
 #-include $(QCPATH)/common/config/rendering-engine.mk
 #ifneq (,$(strip $(wildcard $(PRODUCT_RENDERING_ENGINE_REVLIB))))
 #        MULTI_LANG_ENGINE := REVERIE
