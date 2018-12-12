d586f616b1b84d3e15fa6fe3988ceb45d383587d
Anuj Singhal <sanuj@codeaurora.org>
2018-12-12 12:20:33 +0530

    Add information of vendor HAL's in compatibility matrix
    Change-Id: I62d6061329cd8edf5598d0c2525dc85aaabbabd4

--- msm8996.mk
+++ msm8996.mk
@@ -112,6 +112,7 @@ endif
 DEVICE_MANIFEST_FILE := device/qcom/msm8996/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/msm8996/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/qcom/common/vendor_framework_compatibility_matrix.xml
 
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
